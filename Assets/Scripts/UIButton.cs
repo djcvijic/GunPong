700e7481282e6557bfc4c250fefaac1c2f2f6fa3
Djordje Cvijic <djordje.cvijic.etf@gmail.com>
2022-09-09 12:25:22 +0200

    Scale from center

--- Assets/Scripts/UIButton.cs
+++ Assets/Scripts/UIButton.cs
@@ -13,6 +13,8 @@ public class UIButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 
     private RectTransform rectTransform;
 
+    private Vector3 originalScale;
+
     private void Start()
     {
         rectTransform = GetComponent<RectTransform>();
@@ -22,25 +24,26 @@ public class UIButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
     {
         Input = true;
         OnChange?.Invoke();
-        ScaleFromCenter(rectTransform, scaleWhenPressed);
+        originalScale = rectTransform.localScale;
+        ScaleFromCenter(rectTransform, originalScale * scaleWhenPressed);
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
         Input = false;
         OnChange?.Invoke();
-        ScaleFromCenter(rectTransform, 1f / scaleWhenPressed);
+        ScaleFromCenter(rectTransform, originalScale);
     }
 
-    private static void ScaleFromCenter(RectTransform t, float amount)
+    private static void ScaleFromCenter(RectTransform t, Vector3 newScale)
     {
         var rect = t.rect;
         var pivot = t.pivot;
         var scale = t.localScale;
-        var newScale = scale * amount;
         var offset = 0.5f * (scale - newScale);
         offset.x *= rect.width * (1f - 2f * pivot.x);
         offset.y *= rect.height * (1f - 2f * pivot.y);
+        offset.z = 0;
         t.localPosition += offset;
         t.localScale = newScale;
     }
