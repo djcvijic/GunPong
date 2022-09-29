55d5851344d2cdd9b45292a6eef1e5fcd59d00a3
Djordje Cvijic <djordje.cvijic.etf@gmail.com>
2022-09-29 17:11:40 +0200

    Simplify GameBounds

--- Assets/Scripts/Logic/Core/GameBounds.cs
+++ Assets/Scripts/Logic/Core/GameBounds.cs
@@ -1,12 +1,11 @@
 using UnityEngine;
-using Util.Extensions;
 using View.Common;
 
 namespace Logic.Core
 {
     public class GameBounds
     {
-        private readonly Transform transform;
+        private readonly Transform gameBoundsTransform;
 
         private readonly Transform paddedBoundsTransform;
 
@@ -16,9 +15,10 @@ namespace Logic.Core
 
         private readonly bool constrainZ;
 
-        public GameBounds(Transform transform, Transform paddedBoundsTransform, bool constrainX, bool constrainY, bool constrainZ)
+        public GameBounds(Transform gameBoundsTransform, Transform paddedBoundsTransform,
+            bool constrainX, bool constrainY, bool constrainZ)
         {
-            this.transform = transform;
+            this.gameBoundsTransform = gameBoundsTransform;
             this.paddedBoundsTransform = paddedBoundsTransform;
             this.constrainX = constrainX;
             this.constrainY = constrainY;
@@ -27,33 +27,31 @@ namespace Logic.Core
 
         public bool KeepInBoundsPadded(Vector3 position, Vector3 scale, out Vector3 clampedPosition)
         {
-            var result = IsLeavingBounds(transform, position, scale,
-                paddedBoundsTransform.localScale, constrainX, constrainY, constrainZ,
-                out clampedPosition, out _);
-            return result != GameBoundsEdge.None;
+            return IsLeavingBounds(position, scale, true, out clampedPosition, out _) != GameBoundsEdge.None;
         }
 
         public GameBoundsEdge Reflect(Vector3 position, Vector3 scale, out Vector3 reflectedPosition)
         {
-            var result = IsLeavingBounds(transform, position, scale,
-                Vector3.one, constrainX, constrainY, constrainZ,
-                out _, out reflectedPosition);
-            return result;
+            return IsLeavingBounds(position, scale, false, out _, out reflectedPosition);
         }
 
-        private static GameBoundsEdge IsLeavingBounds(Transform bounds, Vector3 position, Vector3 scale,
-            Vector3 paddedScale, bool constrainX, bool constrainY, bool constrainZ,
+        public bool IsOutOfBounds(Vector3 position, Vector3 scale)
+        {
+            return IsLeavingBounds(position, -scale, false, out _, out _) != GameBoundsEdge.None;
+        }
+
+        private GameBoundsEdge IsLeavingBounds(Vector3 position, Vector3 scale, bool usePadding,
             out Vector3 clampedPosition, out Vector3 reflectedPosition)
         {
-            var boundsPosition = bounds.position;
-            var boundsScale = bounds.localScale;
+            var boundsPosition = gameBoundsTransform.position;
+            var boundsScale = usePadding ? paddedBoundsTransform.lossyScale : gameBoundsTransform.lossyScale;
             var result = GameBoundsEdge.None;
             clampedPosition = position;
             reflectedPosition = position;
 
             if (constrainX)
             {
-                var minX = boundsPosition.x - 0.5f * paddedScale.x * boundsScale.x + 0.5f * scale.x;
+                var minX = boundsPosition.x - 0.5f * boundsScale.x + 0.5f * scale.x;
                 if (position.x < minX)
                 {
                     clampedPosition.x = minX;
@@ -62,7 +60,7 @@ namespace Logic.Core
                 }
                 else
                 {
-                    var maxX = boundsPosition.x + 0.5f * paddedScale.x * boundsScale.x - 0.5f * scale.x;
+                    var maxX = boundsPosition.x + 0.5f * boundsScale.x - 0.5f * scale.x;
                     if (position.x > maxX)
                     {
                         clampedPosition.x = maxX;
@@ -74,7 +72,7 @@ namespace Logic.Core
 
             if (constrainY)
             {
-                var minY = boundsPosition.y - 0.5f * paddedScale.y * boundsScale.y + 0.5f * scale.y;
+                var minY = boundsPosition.y - 0.5f * boundsScale.y + 0.5f * scale.y;
                 if (position.y < minY)
                 {
                     clampedPosition.y = minY;
@@ -83,7 +81,7 @@ namespace Logic.Core
                 }
                 else
                 {
-                    var maxY = boundsPosition.y + 0.5f * paddedScale.y * boundsScale.y - 0.5f * scale.y;
+                    var maxY = boundsPosition.y + 0.5f * boundsScale.y - 0.5f * scale.y;
                     if (position.y > maxY)
                     {
                         clampedPosition.y = maxY;
@@ -95,7 +93,7 @@ namespace Logic.Core
 
             if (constrainZ)
             {
-                var minZ = boundsPosition.z - 0.5f * paddedScale.z * boundsScale.z + 0.5f * scale.z;
+                var minZ = boundsPosition.z - 0.5f * boundsScale.z + 0.5f * scale.z;
                 if (position.z < minZ)
                 {
                     clampedPosition.z = minZ;
@@ -104,56 +102,13 @@ namespace Logic.Core
                 }
                 else
                 {
-                    var maxZ = boundsPosition.z + 0.5f * paddedScale.z * boundsScale.z - 0.5f * scale.z;
+                    var maxZ = boundsPosition.z + 0.5f * boundsScale.z - 0.5f * scale.z;
                     if (position.z > maxZ)
                     {
                         clampedPosition.z = maxZ;
                         reflectedPosition.z = 2f * maxZ - position.z;
                         result = GameBoundsEdge.Front;
                     }
-                }
-            }
-
-            return result;
-        }
-
-        public bool IsOutOfBounds(Vector3 position, Vector3 scale)
-        {
-            var t = transform;
-            var boundsPosition = t.position;
-            var boundsScale = t.localScale;
-            var result = false;
-
-            if (constrainX)
-            {
-                var minX = boundsPosition.x - 0.5f * boundsScale.x - 0.5f * scale.x;
-                var maxX = boundsPosition.x + 0.5f * boundsScale.x + 0.5f * scale.x;
-                var clampedX = Mathf.Clamp(position.x, minX, maxX);
-                if (!clampedX.Approximately(position.x))
-                {
-                    result = true;
-                }
-            }
-
-            if (constrainY)
-            {
-                var minY = boundsPosition.y - 0.5f * boundsScale.y - 0.5f * scale.y;
-                var maxY = boundsPosition.y + 0.5f * boundsScale.y + 0.5f * scale.y;
-                var clampedY = Mathf.Clamp(position.y, minY, maxY);
-                if (!clampedY.Approximately(position.y))
-                {
-                    result = true;
-                }
-            }
-
-            if (constrainZ)
-            {
-                var minZ = boundsPosition.z - 0.5f * boundsScale.z - 0.5f * scale.z;
-                var maxZ = boundsPosition.z + 0.5f * boundsScale.z + 0.5f * scale.z;
-                var clampedZ = Mathf.Clamp(position.z, minZ, maxZ);
-                if (!clampedZ.Approximately(position.z))
-                {
-                    result = true;
                 }
             }
 
